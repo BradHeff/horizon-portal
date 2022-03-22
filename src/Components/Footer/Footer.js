fc8c50a09b834242e4052a0ffba2b030501572ae
Brad Heffernan <ph3onix83@gmail.com>
2022-03-22 19:00:13 +1030

    update

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,17 +4,19 @@ const Footer = (props) => {
   return (
     <div className='footer bg-footer'>
         <span>Copywrite ©<a href="https://www.horizon.sa.edu.au" className='btn btn-link'>Horizon Christian School</a>2022</span>
-        <div className='columns align-items-center justify-content-center'>
-            <div className='column col-4'>
+        <div className='container grid-xl'>
+        <div className='columns align-items-center justify-content-center mt-2'>
+            <div className='column col-md-12 col-4 text-center'>
                 <p>JOURNEY FAITHFULLY WITH GOD</p>
             </div>
-            <div className='column col-4'>
+            <div className='column col-md-12 col-4 text-center'>
                 <p>INSPIRE A LOVE OF LEARNING</p>
             </div>
-            <div className='column col-4'>
+            <div className='column col-md-12 col-4 text-center'>
                 <p>NURTURE PARTNERSHIPS IN COMMUNITY</p>
             </div>
 
+        </div>
         </div>
     </div>
   );
