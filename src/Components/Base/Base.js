11260269536d39a72012cf4b029d20ab6674c03a
Brad Heffernan <ph3onix83@gmail.com>
2022-03-25 09:34:39 +1030

    updated

--- src/Components/Base/Base.js
+++ src/Components/Base/Base.js
@@ -9,17 +9,17 @@ const Base = (props) => {
   return (
     <div className='p-relative'>     
     <div className='App-header overlay'  style={{minHeight: 'calc(100vh - 197px)'}}>
-      <span className='support'><a href='mailto:djohnson@horizon.sa.edu.au?cc=bheffernan@horizon.sa.edu.au&subject=Horizon Canvas Choice Page Problem'>Contact Support</a></span>
+      <span className='support'><a className='btn btn-panel' href='mailto:djohnson@horizon.sa.edu.au?cc=bheffernan@horizon.sa.edu.au&subject=Horizon Canvas Choice Page Problem'>Contact Support</a></span>
       <div className='container grid-xl z-index'>
       <div className='ribbon z-index'>Released Wed 30th March</div>
 
       {/* <img src={release} alt={release} className='img-responsive' style={{marginLeft: 'auto', marginRight: 'auto'}}/> */}
-        <div className='columns align-items-center justify-content-center mt-2'>
+        <div className='columns align-items-center justify-content-center mt-mb-3'>
           
           <div className='column col-md-10 col-4 col-mx-auto mb-2'>
             <div className='click-box-student'>
                 <Panels title="Canvas Student" choice="KIDS" delay="0">
-                    <p>Access to the students portal of the Horizon Christian School Canvas</p>
+                    <p>Access to Canvas for Horizon Christian School Students</p>
                     {/* <a href='https://horizon-sa.instructure.com' className='btn btn-panel'>Go To Canvas</a> */}
                     <a href='#0' className='btn btn-panel'>Go To Canvas</a>
                 </Panels>
