375b20bf4f0b6cc994cc5b55eedbbe430bd2a266
Brad Heffernan <ph3onix83@gmail.com>
2022-05-12 10:42:45 +0930

    updated

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Footer = (props) => {
+  var date = new Date().getFullYear();
   return (
     <div className='footer bg-footer'>
-        <span>Copywrite ©<a href="https://www.horizon.sa.edu.au" className='btn btn-link'>Horizon Christian School</a>2022</span>        
+        <span>Copyright ©<a href="https://www.horizon.sa.edu.au" className='btn btn-link'>Horizon Christian School</a>{date}</span>        
     </div>
   );
 }
