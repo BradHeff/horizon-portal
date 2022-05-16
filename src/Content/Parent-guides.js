f3a3f9d2af395c9bdf4c6c107833cf6a1e194e41
Dan <105445965+IsraeDan@users.noreply.github.com>
2022-05-16 12:39:58 +0930

    Update Parent-guides.js

--- src/Content/Parent-guides.js
+++ src/Content/Parent-guides.js
@@ -21,27 +21,27 @@ render() {
               </div>
                 
                   {/*ROW 1*/}
-                  <Guides trans={true} color="light" title="How To" titleColor="Access" title2="The App" image={diagram} link="#0">
+                  <Guides trans={true} color="light" title="How to Access" titleColor="Canvas Parent" title2="The App" image={diagram} link="#0">
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s                  
                   </Guides>                 
                   {/*ROW 2*/}
-                  <Guides trans={true} color="dark" bg="bg-light" title="How To" titleColor="Access" title2="The App" image={diagram} link="#0">
+                  <Guides trans={true} color="dark" bg="bg-light" title="How to Access" titleColor="Canvas Parent" title2="Online" image={diagram} link="#0">
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                   </Guides>
                   {/*ROW 3*/}                  
-                  <Guides trans={true} color="footer" bg="bg-white" title="How To" titleColor="Access" title2="The App" image={diagram} link="#0">
+                  <Guides trans={true} color="footer" bg="bg-white" title="How to Access the" titleColor="Horizon Parent Portal" title2="in Canvas Parent The App" image={diagram} link="#0">
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                   </Guides>
                   {/*ROW 4*/}                  
-                  <Guides trans={true} color="footer" bg="bg-white" title="How To" titleColor="Access" title2="The App" image={diagram} link="#0">
+                  <Guides trans={true} color="light" bg="bg-light" title="How To" titleColor="Add a Second Child" title2=" to Canvas Parent The App" image={diagram} link="#0">
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                   </Guides>
                   {/*ROW 5*/}                  
-                  <Guides trans={true} color="footer" bg="bg-white" title="How To" titleColor="Access" title2="The App" image={diagram} link="#0">
+                  <Guides trans={true} color="dark" bg="bg-white" title="How To" titleColor="Use Canvas Parent" title2="The App" image={diagram} link="#0">
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                   </Guides>
                   {/*ROW 6*/}                  
-                  <Guides trans={true} color="footer" bg="bg-white" title="How To" titleColor="Access" title2="The App" image={diagram} link="#0">
+                  <Guides trans={true} color="footer" bg="bg-light" title="How To" titleColor="Change Notification Settings" title2="in Canvas Parent The App" image={diagram} link="#0">
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                   </Guides>
               </div>
