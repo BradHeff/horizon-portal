2ba777b0a700137d6144dc2ca4a495f0918887dc
Dan <105445965+IsraeDan@users.noreply.github.com>
2022-05-16 13:20:28 +0930

    Update Parent-guides.js

--- src/Content/Parent-guides.js
+++ src/Content/Parent-guides.js
@@ -22,27 +22,27 @@ render() {
                 
                   {/*ROW 1*/}
                   <Guides trans={true} color="light" title="How to Access" titleColor="Canvas Parent" title2="The App" image={diagram} link="#0">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s                  
+                    Learn how to download the Canvas Parent App and create an account linked to your child.
                   </Guides>                 
                   {/*ROW 2*/}
                   <Guides trans={true} color="dark" bg="bg-light" title="How to Access" titleColor="Canvas Parent" title2="Online" image={diagram} link="#0">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    Learn how to access the online version of Canvas Parent to see more detail about your children's progress.
                   </Guides>
                   {/*ROW 3*/}                  
                   <Guides trans={true} color="footer" bg="bg-white" title="How to Access the" titleColor="Horizon Parent Portal" title2="in Canvas Parent The App" image={diagram} link="#0">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    Access the Horizon Parent Portal to find a growing number of school related links, guides and apps.
                   </Guides>
                   {/*ROW 4*/}                  
                   <Guides trans={true} color="light" bg="bg-light" title="How To" titleColor="Add a Second Child" title2=" to Canvas Parent The App" image={diagram} link="#0">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    Learn how to add all your children to the Canvas Parent App to follow their progress.
                   </Guides>
                   {/*ROW 5*/}                  
                   <Guides trans={true} color="dark" bg="bg-white" title="How To" titleColor="Use Canvas Parent" title2="The App" image={diagram} link="#0">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    The Basic How to Guide for the Canvas Parent App.
                   </Guides>
                   {/*ROW 6*/}                  
                   <Guides trans={true} color="footer" bg="bg-light" title="How To" titleColor="Change Notification Settings" title2="in Canvas Parent The App" image={diagram} link="#0">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    Customise your notifications so you only see the things that are important for you.
                   </Guides>
               </div>
         </div>
